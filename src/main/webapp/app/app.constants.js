3b7cd22f98d257aa3487c30178ae244d5ef62c2d
khirulbasher <khirulbasherbsmrstu@gmail.com>
2018-02-16 22:53:06 +0600

    Production mode success

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('projectApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1518788280545)
+        .constant('BUILD_TIMESTAMP', 1518799398327)
 ;
 })();
